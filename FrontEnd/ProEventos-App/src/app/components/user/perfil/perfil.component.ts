6a814d264d6410e4179649fb4921162ab24af77d
Bernardo <bernardo@sobit.com.br>
2022-10-03 15:29:11 -0300

    Parte da Seção 8 (consertar perfil)

--- FrontEnd/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ FrontEnd/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
 
-  ngOnInit() {
+  get f(): any {
+    return this.form.controls;
+  }
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void { this.validation() }
+
+  private validation(): void {
+
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('password', 'confirmPassword')
+    };
+
+    this.form = this.fb.group({
+      //title: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      telephone: ['', Validators.required],
+      function: ['', Validators.required],
+      description: ['', Validators.required]
+    }, formOptions);
   }
 
 }
